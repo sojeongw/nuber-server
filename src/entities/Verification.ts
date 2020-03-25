8820ac90bc9bea40057e6e5fce4e77bacdd4519b
sojeongw <redthink90@gmail.com>
2020-03-25 20:50:17 +0900

    #1.21 Place, Ride Entity

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -1,31 +1,27 @@
-import {
-    BaseEntity,
-    Column,
-    CreateDateColumn,
-    Entity,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn
-} from "typeorm";
-import {verificationTarget} from "../types/types";
+import {BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 
 @Entity()
-class Verification extends BaseEntity {
+class Place extends BaseEntity {
     @PrimaryGeneratedColumn() id: number;
 
-    // mail, Mail 등으로 혼동되지 않도록 타입을 설정한다.
-    @Column({ type: "text", enum: ["PHONE", "EMAIL"] }) // target 타입 뿐만 아니라 여기에도 이중으로 타입을 막아준다.
-    target: verificationTarget;
+    @Column({ type: "text" })
+    name: string;
+
+    @Column({ type: "double precision", default: 0 })
+    lat: number;
+
+    @Column({ type: "double precision", default: 0 })
+    lng: number;
 
     @Column({ type: "text" })
-    payload: string;
-
-    @Column({ type: "text" })
-    key: string;
+    address: string;
 
     @Column({ type: "boolean", default: false })
-    used: boolean;
+    isFav: boolean;
 
     @CreateDateColumn() createdAt: string;
     @UpdateDateColumn() updatedAt: string;
+
+
 }
-export default Verification;
+export default Place;
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,2 +1,4 @@
 // mail, Mail 등으로 혼동되지 않도록 타입을 설정한다.
 export type verificationTarget = "PHONE" | "EMAIL";
+
+export type rideStatus = "ACCEPTED" | "FINISHED" | "CANCELED" | "REQUESTING" | "ONROUTE";
