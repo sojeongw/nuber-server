679fb4665ecae515996f3663e5a1e6dedc3f10c4
sojeongw <redthink90@gmail.com>
2020-04-19 21:31:09 +0900

    #1.75 Testing the NearbyRideSubscription

--- src/api/Ride/RequestRide/RequestRide.resolvers.ts
+++ src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -16,21 +16,34 @@ const resolvers: Resolvers = {
                 { req, pubSub }
             ): Promise<RequestRideResponse> => {
                 const user: User = req.user;
-                try {
-                    const ride = await Ride.create({ ...args, passenger: user }).save();
 
-                    // 드라이버에 대한 subscription을 publish 한다.
-                    pubSub.publish("rideRequest", { NearbyRideSubscription: ride });
+                // user가 드라이빙 중이 아니라면 ride를 요청할 수 있다.
+                if (!user.isRiding) {
+                    try {
+                        const ride = await Ride.create({ ...args, passenger: user }).save();
 
-                    return {
-                        ok: true,
-                        error: null,
-                        ride
-                    };
-                } catch (error) {
+                        // 드라이버에 대한 subscription을 publish 한다.
+                        pubSub.publish("rideRequest", { NearbyRideSubscription: ride });
+
+                        user.isRiding = true;
+                        user.save();
+
+                        return {
+                            ok: true,
+                            error: null,
+                            ride
+                        };
+                    } catch (error) {
+                        return {
+                            ok: false,
+                            error: error.message,
+                            ride: null
+                        };
+                    }
+                } else {
                     return {
                         ok: false,
-                        error: error.message,
+                        error: "You can't request two rides",
                         ride: null
                     };
                 }
